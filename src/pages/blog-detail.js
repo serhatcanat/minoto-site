c96e82ba5e39c98755de7073c77765de30c230be
Yunus Tabakoğlu <yunus@positive.com.tr>
2019-05-22 01:32:52 +0300

    blog detay

--- src/pages/blog-detail.js
+++ src/pages/blog-detail.js
@@ -6,17 +6,19 @@ import Loader from 'components/partials/loader'
 import Link from 'components/partials/link'
 import ContentBox from 'components/partials/contentbox'
 import Breadcrumbs from 'components/partials/breadcrumbs'
-import { InputForm, FormInput } from 'components/partials/forms'
 
 // Deps
 import request from 'controllers/request'
-import history from 'controllers/history'
 import { redirect } from 'controllers/navigator'
-import { serializeArray } from 'functions/helpers'
 import parse from 'html-react-parser';
 
 // Assets
-import image_blog_bg  from 'assets/images/blog-bg.svg'
+import image_icon_facebook from 'assets/images/icon/facebook.svg'
+import image_icon_instagram from 'assets/images/icon/instagram.svg'
+import image_icon_youtube from 'assets/images/icon/youtube.svg'
+import image_icon_twitter from 'assets/images/icon/twitter.svg'
+import image_icon_whatsapp from 'assets/images/icon/whatsapp.svg'
+import image_icon_link from 'assets/images/icon/link.svg'
 
 export default class BlogDetail extends React.Component {
 	constructor(props) {
@@ -31,7 +33,6 @@ export default class BlogDetail extends React.Component {
 		let vm = this;
 
 		request.get('/dummy/data/blog-detail.json', {slug: vm.props.match.params.slug}, function(payload, status){
-				console.log(payload);
 			if(payload){
 				vm.setState({
 					blogData: payload
@@ -58,15 +59,92 @@ export default class BlogDetail extends React.Component {
 							<Link href="blog" params={{ action: data.categorySlug }}>{data.category}</Link>
 						</Breadcrumbs>
 
-						<div className="wrapper narrower">
-							<div className="detail-content wysiwyg">
-								<h1 className="content-title">{data.title}</h1>
-								{parse(data.content)}
+						<div className="detail-wrap wrapper narrow">
+							<div className="detail-meta">
+								<span className="meta-date">{data.date}</span>
+							</div>
+
+							<Sharer title={data.title} />
+							<div className="detail-content">
+								<div className="content-text wysiwyg">
+									<h1 className="text-title">{data.title}</h1>
+									{parse(data.content)}
+								</div>
 							</div>
 						</div>
+						{(data.relevant && data.relevant.length) &&
+							<aside className="detail-relevant">
+								<strong className="relevant-title">Diğer Haberler</strong>
+
+								<div className="wrapper narrow">
+									<ul className="relevant-items">
+										{data.relevant.map((article, nth) => (
+										<li className="relevant-item" key={nth}>
+											<ContentBox
+												type="blogpost"
+												pretitle={article.date}
+												title={article.title}
+												image={article.image}
+												url="blogDetail"
+												urlParams={{slug: article.slug}}
+											/>
+										</li>
+										))}
+									</ul>
+								</div>
+							</aside>
+						}
 					</section>
 				}
 			</main>
+		)
+	}
+}
+
+class Sharer extends React.Component {
+	render() {
+		return (
+			<div className="detail-sharer">
+				<strong className="sharer-title">Paylaş</strong>
+
+				<ul className="sharer-opts">
+					<li className="opts-item">
+						<Link type="a" className="sharer-link facebook" href={"https://www.facebook.com/sharer/sharer.php?u=" + window.location.href} target="_blank">
+							<Image src={image_icon_facebook} alt="Facebook" />
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link twitter" href={"http://www.instagram.com/"} target="_blank">
+							<Image src={image_icon_instagram} alt="Instagram" />
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link twitter" href={"http://www.youtube.com/"} target="_blank">
+							<Image src={image_icon_youtube} alt="YouTube" />
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link twitter" href={"http://twitter.com/share?text="+this.props.title+"&url="+window.location.href+"&hashtags=minoto,sifirarac"} target="_blank">
+							<Image src={image_icon_twitter} alt="Twitter" />
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link icon email" href={"mailto:?subject=Bak Minoto'da Ne Buldum!&body=" + this.props.title + window.location.href} target="_blank">
+							<i className="icon-envelope"></i>
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link twitter" href={"http://www.google.com/"} target="_blank">
+							<Image src={image_icon_link} alt="Link Kopyala" />
+						</Link>
+					</li>
+					<li className="opts-item">
+						<Link type="a" className="sharer-link whatsapp" href={"whatsapp://send?text="+window.location.href} data-action="share/whatsapp/share" target="_blank">
+							<Image src={image_icon_whatsapp} alt="WhatsApp" />
+						</Link>
+					</li>
+				</ul>
+			</div>
 		)
 	}
 }
