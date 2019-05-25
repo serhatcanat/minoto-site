1dd0c69d18b6056b943c9e32d306d848be89bde2
Yunus Tabakoğlu <yunus@positive.com.tr>
2019-05-25 20:26:03 +0300

    responsive devam

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -11,6 +11,7 @@ import {formatNumber} from 'functions/helpers'
 
 // Deps
 import axios from 'axios'
+import { connect } from "react-redux";
 
 // Assets
 import image_icon_facebook from 'assets/images/icon/facebook.svg'
@@ -18,7 +19,11 @@ import image_icon_instagram from 'assets/images/icon/instagram.svg'
 import image_icon_youtube from 'assets/images/icon/youtube.svg'
 import image_logo_white from 'assets/images/logo-white.svg'
 
-export default class Footer extends React.Component {
+const mapStateToProps = state => {
+	return { mobile: state.generic.mobile };
+};
+
+class Footer extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
@@ -52,11 +57,12 @@ export default class Footer extends React.Component {
 
 		if(vm.state.brands){
 			brands = [];
-			let groupCount = Math.floor(vm.state.brands.length / 6);
+			let groupCount = (vm.props.mobile ? 2 : 6);
+			let groupSize = Math.ceil(vm.state.brands.length / groupCount);
 
-			for(let k = 0; k < 6; k++){
-				let group = vm.state.brands.slice(k*groupCount, (k*groupCount)+groupCount).map(function(brand, nth){
-					if(((k*groupCount) + nth) < vm.state.brands.length){
+			for(let k = 0; k < groupCount; k++){
+				let group = vm.state.brands.slice(k*groupSize, (k*groupSize)+groupSize).map(function(brand, nth){
+					if(((k*groupSize) + nth) < vm.state.brands.length){
 						return brand;
 					}
 					else{
@@ -70,25 +76,23 @@ export default class Footer extends React.Component {
 		return (
 			<footer className="section footer">
 				{vm.state.brands &&
-					<Responsive type="only-web">
-						<div className="footer-brands">
-							<div className="brands-wrap wrapper narrow">
-								<nav className="brands-list">
-									{brands.map((group, nth) => (
-										<div className="list-group" key={nth}>
-											{group.map((brand, nt) => (
-												<div className="brands-item" key={nt}>
-													<Link href={brand.url}>
-														{brand.title} {brand.count && <span className="item-count">({formatNumber(brand.count)})</span>}
-													</Link>
-												</div>
-											))}
-										</div>
-									))}
-								</nav>
-							</div>
+					<div className="footer-brands">
+						<div className="brands-wrap wrapper narrow">
+							<nav className="brands-list">
+								{brands.map((group, nth) => (
+									<div className="list-group" key={nth}>
+										{group.map((brand, nt) => (
+											<div className="brands-item" key={nt}>
+												<Link href={brand.url}>
+													{brand.title} {brand.count && <span className="item-count">({formatNumber(brand.count)})</span>}
+												</Link>
+											</div>
+										))}
+									</div>
+								))}
+							</nav>
 						</div>
-					</Responsive>
+					</div>
 				}
 				<div className="footer-bottom">
 					<div className="footer-wrap wrapper narrow">
@@ -98,42 +102,79 @@ export default class Footer extends React.Component {
 								</a>
 						</div>
 						<nav className="footer-col footer-nav">
-							<ul className="nav-col">
-								<li>
-									<a className="footer-link" href="/">Markalar</a>
-								</li>
-								<li>
-									<Link className="footer-link" href="dealers">Bayiler</Link>
-								</li>
-								<li>
-									<Link className="footer-link" href="sitemap">Site Haritası</Link>
-								</li>
-							</ul>
-							<ul className="nav-col">
-								<li>
-									<Link className="footer-link" href="about">Hakkında</Link>
-								</li>
-								<li>
-									<Link className="footer-link" href="blog">Blog</Link>
-								</li>
-								<li>
-									<Link className="footer-link" href="contact" />
-								</li>
-							</ul>
-							<ul className="nav-col">
-								<li>
-									<Link className="footer-link" href="faq">Yardım</Link>
-								</li>
-								<li>
-									<Link className="footer-link" href="faq" />
-								</li>
-								<li>
-									<Link className="footer-link" href="privacy" />
-								</li>
-							</ul>
+							<Responsive>
+								<ul className="nav-col">
+									<li>
+										<a className="footer-link" href="/">Markalar</a>
+									</li>
+									<li>
+										<Link className="footer-link" href="dealers">Bayiler</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="sitemap">Site Haritası</Link>
+									</li>
+								</ul>
+								<ul className="nav-col">
+									<li>
+										<a className="footer-link" href="/">Markalar</a>
+									</li>
+									<li>
+										<Link className="footer-link" href="dealers">Bayiler</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="about">Hakkında</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="blog">Blog</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="sitemap">Site Haritası</Link>
+									</li>
+								</ul>
+							</Responsive>
+							<Responsive>
+								<ul className="nav-col">
+									<li>
+										<Link className="footer-link" href="about">Hakkında</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="blog">Blog</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="contact" />
+									</li>
+								</ul>
+								<ul className="nav-col">
+									<li>
+										<Link className="footer-link" href="contact" />
+									</li>
+									<li>
+										<Link className="footer-link" href="faq">Yardım</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="faq" />
+									</li>
+									<li>
+										<Link className="footer-link" href="privacy" />
+									</li>
+								</ul>
+							</Responsive>
+							<Responsive type="only-web">
+								<ul className="nav-col">
+									<li>
+										<Link className="footer-link" href="faq">Yardım</Link>
+									</li>
+									<li>
+										<Link className="footer-link" href="faq" />
+									</li>
+									<li>
+										<Link className="footer-link" href="privacy" />
+									</li>
+								</ul>
+							</Responsive>
 							<div className="nav-col nav-contact">
 								<div className="contact-text">
-									<p className="contact-title">İletişim için</p>
+									<p className="contact-title">İletişim</p>
 									<p className="contact-phone"><a href="tel:08002129898" title="Telefon"><i className="icon-phone"></i> 0800 212 98 98</a></p>
 									<p className="contact-phone"><a href="mailto:info@minoto.com" title="E-Posta"><i className="icon-envelope"></i> info@minoto.com</a></p>
 								</div>
@@ -158,16 +199,20 @@ export default class Footer extends React.Component {
 							</div>
 						</nav>
 
+						{vm.props.mobile &&
+							<FooterSubscription />
+						}
+
 						<div className="footer-col">
 							<div className="footer-app">
 								<h3 className="app-title">Mobil uygulamamızı indirdiniz mi?</h3>
-								<Btn tag="a" className="app-link" white href="http://www.google.com" target="_blank" rel="noopener noreferrer" icon="appstore">Apple Store</Btn>
-								<Btn tag="a" className="app-link" white href="http://www.google.com" target="_blank" rel="noopener noreferrer" icon="playstore">Google Play</Btn>
+								<Btn tag="a" className="app-link" white {...(vm.props.mobile ? {low: true} : {})} href="http://www.google.com" target="_blank" rel="noopener noreferrer" icon="appstore">Apple Store</Btn>
+								<Btn tag="a" className="app-link" white {...(vm.props.mobile ? {low: true} : {})} href="http://www.google.com" target="_blank" rel="noopener noreferrer" icon="playstore">Google Play</Btn>
 							</div>
 
-							<form className="footer-subscription">
-								<input type="text" className="subscription-input" placeholder="E-postanızı yazın, bizden haberler verelim." />
-							</form>
+							{!vm.props.mobile &&
+								<FooterSubscription />
+							}
 						</div>
 					</div>
 					<div className="footer-copyright footer-wrap wrapper narrow">
@@ -180,6 +225,18 @@ export default class Footer extends React.Component {
 					</div>
 				</div>
 			</footer>
+		)
+	}
+}
+
+export default connect(mapStateToProps)(Footer);
+
+class FooterSubscription extends React.Component {
+	render() {
+		return (
+			<form className="footer-subscription">
+				<input type="text" className="subscription-input" placeholder="E-postanızı yazın, bizden haberler verelim." />
+			</form>
 		)
 	}
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,10 @@ import React from 'react'
 // Partials
 import Image from 'components/partials/image'
 import Link from 'components/partials/link'
+import Slider from 'components/partials/slider'
 
 // Deps
+import { connect } from "react-redux";
 
 // Assets
 import image_icon_linkedin from 'assets/images/icon/linkedin.svg'
@@ -14,9 +16,16 @@ import image_icon_linkedin from 'assets/images/icon/linkedin.svg'
 //import image_icon_instagram from 'assets/images/icon/instagram.svg'
 //import image_icon_youtube from 'assets/images/icon/youtube.svg'
 
-export default class About extends React.Component {
+const mapStateToProps = state => {
+	return { mobile: state.generic.mobile };
+};
+
+class About extends React.Component {
 
 	render () {
+		let GalleryContainer = (this.props.mobile ? Slider : 'div');
+		let galleryProps = (this.props.mobile ? {slides: 2} : {});
+
 		return (
 			<main className="page about">
 				<section className="section about-head">
@@ -38,22 +47,22 @@ export default class About extends React.Component {
 					<div className="counts-wrap wrapper narrow">
 						<div className="counts-item">
 							<span className="item-num">1.5M</span>
-							<strong className="item-title">Toplam Bireysel Üye</strong>
+							<strong className="item-title">Toplam <br className="only-mobile" />Bireysel Üye</strong>
 						</div>
 						<div className="counts-item">
 							<span className="item-num">2.8M</span>
-							<strong className="item-title">Aylık Farklı Ziyaretçi</strong>
+							<strong className="item-title">Aylık Farklı <br className="only-mobile" />Ziyaretçi</strong>
 						</div>
 						<div className="counts-item">
 							<span className="item-num">38M</span>
-							<strong className="item-title">Aylık Toplam Sayfa Göüntülenmesi</strong>
+							<strong className="item-title">Aylık Toplam Sayfa <br className="only-mobile" />Görüntülenmesi</strong>
 						</div>
 					</div>
 				</section>
 
 				<section className="section about-brands">
 					<div className="brands-wrap wrapper narrow">
-						<Image className="brands-image" src="/dummy/images/about-imgs.png" />
+						<Image className="brands-image" src="/dummy/images/about-imgs.png" mobile="/dummy/images/about-imgs-mobile.png" />
 
 						<div className="brands-content">
 							<h3 className="content-title">100'den fazla otomobil markası ve binlerce bayi minoto'da</h3>
@@ -68,61 +77,63 @@ export default class About extends React.Component {
 				<section className="section about-team">
 					<h3 className="team-title">Ekibimiz</h3>
 					<div className="team-wrap wrapper narrow">
-						<div className="team-member">
-							<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Kaan Boyner" />
-							<div className="member-content">
-								<strong className="member-position">CEO</strong>
-								<h4 className="member-name">Kaan Boyner</h4>
+						<GalleryContainer className="team-container" {...galleryProps}>
+							<div className="team-member">
+								<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Kaan Boyner" />
+								<div className="member-content">
+									<strong className="member-position">CEO</strong>
+									<h4 className="member-name">Kaan Boyner</h4>
 
-								<div className="member-contact">
-									<Link tag="a" title="Kaan Boyner LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
-										<Image src={image_icon_linkedin} alt="LinkedIn" />
-									</Link>
+									<div className="member-contact">
+										<Link tag="a" title="Kaan Boyner LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
+											<Image src={image_icon_linkedin} alt="LinkedIn" />
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div className="team-member">
-							<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Ferhat Albayrak" />
-							<div className="member-content">
-								<strong className="member-position">CEO</strong>
-								<h4 className="member-name">Ferhat Albayrak</h4>
+							<div className="team-member">
+								<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Ferhat Albayrak" />
+								<div className="member-content">
+									<strong className="member-position">CEO</strong>
+									<h4 className="member-name">Ferhat Albayrak</h4>
 
-								<div className="member-contact">
-									<Link tag="a" title="Ferhat Albayrak LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
-										<Image src={image_icon_linkedin} alt="LinkedIn" />
-									</Link>
+									<div className="member-contact">
+										<Link tag="a" title="Ferhat Albayrak LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
+											<Image src={image_icon_linkedin} alt="LinkedIn" />
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div className="team-member">
-							<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Kaan Boyner" />
-							<div className="member-content">
-								<strong className="member-position">CEO</strong>
-								<h4 className="member-name">Kaan Boyner</h4>
+							<div className="team-member">
+								<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Kaan Boyner" />
+								<div className="member-content">
+									<strong className="member-position">CEO</strong>
+									<h4 className="member-name">Kaan Boyner</h4>
 
-								<div className="member-contact">
-									<Link tag="a" title="Kaan Boyner LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
-										<Image src={image_icon_linkedin} alt="LinkedIn" />
-									</Link>
+									<div className="member-contact">
+										<Link tag="a" title="Kaan Boyner LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
+											<Image src={image_icon_linkedin} alt="LinkedIn" />
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
 
-						<div className="team-member">
-							<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Ferhat Albayrak" />
-							<div className="member-content">
-								<strong className="member-position">CEO</strong>
-								<h4 className="member-name">Ferhat Albayrak</h4>
+							<div className="team-member">
+								<Image className="member-image" src="/dummy/images/profile-picture.jpg" bg alt="Ferhat Albayrak" />
+								<div className="member-content">
+									<strong className="member-position">CEO</strong>
+									<h4 className="member-name">Ferhat Albayrak</h4>
 
-								<div className="member-contact">
-									<Link tag="a" title="Ferhat Albayrak LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
-										<Image src={image_icon_linkedin} alt="LinkedIn" />
-									</Link>
+									<div className="member-contact">
+										<Link tag="a" title="Ferhat Albayrak LinkedIn Profili" href="http://www.linkedin.com" target="_blank" rel="nofollow">
+											<Image src={image_icon_linkedin} alt="LinkedIn" />
+										</Link>
+									</div>
 								</div>
 							</div>
-						</div>
+						</GalleryContainer>
 					</div>
 				</section>
 			</main>
@@ -130,3 +141,5 @@ export default class About extends React.Component {
 		)
 	}
 }
+
+export default connect(mapStateToProps)(About);
