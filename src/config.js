cc67c305f2af148f1e60b7d7e8e31f2ca384307f
tallrye <s_uzuncavdar@hotmail.com>
2019-06-19 18:58:21 +0300

    API url for test env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const apiBase = 'http://localhost:8000/v1/shared/'
+export const apiBase = 'http://minoto-api.thinkerfox.com/v1/shared/'
 export const storagePath = 'https://minoto-test.ams3.digitaloceanspaces.com/'
 export const basePath = 'https://minoto.com'
