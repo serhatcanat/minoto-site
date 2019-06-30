9ee35b61363d2aa75c6ee3d2fc12116731cdf6db
Yunus Tabakoğlu <yunus@positive.com.tr>
2019-06-30 21:24:23 +0300

    decimals

--- src/components/partials/price-tag.js
+++ src/components/partials/price-tag.js
@@ -10,7 +10,7 @@ import { formatNumber } from 'functions/helpers'
 export default class PriceTag extends React.Component {
 
 	render() {
-		let priceData = formatNumber(this.props.price, {showDecimals: true}).split(',');
+		let priceData = formatNumber(this.props.price, {showDecimals: false}).split(',');
 		return (
 			<span className={"pricetag " + this.props.className}>
 				<span className="pricetag-base">{priceData[0]}</span>{priceData[1] && <React.Fragment><span className="pricetag-decimal">,{priceData[1]}</span></React.Fragment>} <span className="pricetag-currency">TL</span>
