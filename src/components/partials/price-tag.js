7c7e56f945a535b6fa133c12f2659c0774551bad
Yunus Tabakoğlu <yunus@positive.com.tr>
2019-06-30 19:24:01 +0300

    ..

---
+++ src/components/partials/price-tag.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Partials
+import PopInfo from 'components/partials/popinfo.js';
+import Link from 'components/partials/link.js';
+
+// Deps
+import omit from 'lodash/omit'
+import clone from 'lodash/clone'
+
+// Assets
+
+export default class PriceTag extends React.Component {
+
+	render() {
+		<div className="price-tag"
+	}
+}
