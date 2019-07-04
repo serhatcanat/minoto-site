61c6f8af9c602c5562ed8b34273d76b1820bbaa5
tallrye <s_uzuncavdar@hotmail.com>
2019-07-04 12:18:08 +0300

    Dev test API link

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //const domain = (window.location.hostname === 'localhost' ? 'http://localhost:8000' : 'https://api.minoto.com');
 
-const domain = 'https://api.minoto.com';
+const domain = 'http://minoto-api.thinkerfox.com';
 
 export const apiBase = `${domain}/v1/shared/`
 export const storagePath = 'https://minoto-test.ams3.digitaloceanspaces.com/'
