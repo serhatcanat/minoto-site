d9eafe2c4f70cc528db18b1ab3f544ef19db915e
Yunus Tabakoğlu <yunus@positive.com.tr>
2019-07-06 15:01:07 +0300

    ..

--- src/config.js
+++ src/config.js
@@ -1,17 +1,19 @@
 let domain = window.location.hostname
 
-let endpoint = ''
+let endpoint = process.env.REACT_APP_API_ENDPOINT ? process.env.REACT_APP_API_ENDPOINT : false;
 
-switch (domain) {
-    case 'localhost':
-        endpoint = 'http://localhost:8000'
-        break;
-    case 'minoto.thinkerfox.com':
-        endpoint = 'http://minoto-api.thinkerfox.com'
-        break;
-    default:
-        endpoint = 'https://api.minoto.com'
-        break;
+if(!endpoint){
+	switch (domain) {
+	    case 'localhost':
+	        endpoint = 'http://localhost:8000'
+	        break;
+	    case 'minoto.thinkerfox.com':
+	        endpoint = 'http://minoto-api.thinkerfox.com'
+	        break;
+	    default:
+	        endpoint = 'https://api.minoto.com'
+	        break;
+	}
 }
 
 export const apiBase = `${endpoint}/v1/shared/`
