ebfd226cea4ef362b20e5fe1551f2a746f2f889b
Mehmet Yıldırım <mehmetyildi@msn.com>
2019-10-30 12:07:34 +0300

    Scroll test added fixes #9

--- cypress/integration/login/basic.spec.js
+++ cypress/integration/login/basic.spec.js
@@ -3,19 +3,19 @@
 context('Aliasing', () => {
     beforeEach(() => {
         cy.visit('localhost:3000')
-    })
+    });
 
     describe('Basic Test', () => {
         it('.should() - make an assertion about the current subject', () => {
             cy.get('.intro-title span')
                 .should('have.class', 'colored')
-        })
+        });
 
 
         it('.should() - login model display none', () => {
             cy.get('.modal-login')
                 .should('not.have.class', 'show')
-        })
+        });
 
 
         it('.should() - login model display', () => {
@@ -26,7 +26,7 @@ context('Aliasing', () => {
 
             cy.get('.modal-login')
                 .should('have.class', 'show');
-        })
+        });
 
         it('.should() - login form fail', () => {
             cy.get('button.nav-link  ~ button.nav-link')
@@ -35,10 +35,10 @@ context('Aliasing', () => {
             cy.wait(200);
 
             cy.get('.form.loginform-form [name="email"]').first()
-                .type("gulsah@thinkerfox.com")
+                .type("gulsah@thinkerfox.com");
 
             cy.get('.form.loginform-form [name="password"]').first()
-                .type("thinkerfox111")
+                .type("thinkerfox111");
 
             cy.get(".loginform-form .btn-content")
                 .click();
@@ -47,11 +47,32 @@ context('Aliasing', () => {
 
             cy.get(".form.loginform-form")
                 .find("div.loginform-message.error")
-                .should("have.text", "Böyle bir kayıt yok");
+                .should("exist");
+        });
 
+        it('.should() - pass login', () => {
+            cy.get('button.nav-link  ~ button.nav-link')
+                .click();
 
+            cy.wait(200);
 
+            cy.get('.form.loginform-form [name="email"]').first()
+                .type("gulsah@thinkerfox.com");
+
+            cy.get('.form.loginform-form [name="password"]').first()
+                .type("minoto121");
+
+            cy.get(".loginform-form .btn-content")
+                .click();
+            cy.get(".form.loginform-form")
+                .find("div.loginform-message.success")
+                .should("exist");
+            cy.wait(400);
+
+            cy.get("div.imagewrap.image-iconloader")
+                .find("div")
+                .should("have.class", "imagewrap-image");
         })
 
     })
-})
+});
