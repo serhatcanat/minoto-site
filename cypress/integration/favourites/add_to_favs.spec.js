31db26ab8431fb45875d83fbbb3eca151b5395a1
Mehmet Yıldırım <mehmetyildi@msn.com>
2019-12-03 14:04:55 +0300

    test optimizasyonu yapıldı, login testi, favoriler spec'inde test edildiği için kaldırıldı

--- cypress/integration/favourites/add_to_favs.spec.js
+++ cypress/integration/favourites/add_to_favs.spec.js
@@ -1,29 +1,24 @@
 context('Add to Favourites', () => {
-    beforeEach(() => {
-        cy.visit('localhost:3000')
-        cy.get('button.nav-link  ~ button.nav-link')
-            .click();
 
-        cy.wait(200);
-
-        cy.get('.form.loginform-form [name="email"]').first()
-            .type("gulsah@thinkerfox.com");
-
-        cy.get('.form.loginform-form [name="password"]').first()
-            .type("minoto121");
-
-        cy.get(".loginform-form .btn-content")
-            .click();
-        cy.get(".form.loginform-form")
-            .find("div.loginform-message.success")
-        cy.wait(1000);
-
-        cy.get("div.imagewrap.image-iconloader")
-            .find("div")
-    });
 
     describe('Add to favourites to authenticated client profile', () => {
-        it('.should() - add to favourites list of authenticated user when the user clicks on favourites button', () => {
+        it('.should() - add to favourites list of authenticated user when the user clicks on favourites button', ()=>{
+                cy.visit('localhost:3000')
+                cy.get('button.nav-link  ~ button.nav-link')
+                    .click();
+
+                cy.wait(200);
+
+                cy.get('.form.loginform-form [name="email"]').first()
+                    .type("gulsah@thinkerfox.com");
+
+                cy.get('.form.loginform-form [name="password"]').first()
+                    .type("minoto121");
+
+                cy.get(".loginform-form .btn-content")
+                    .click();
+
+                cy.wait(1000);
             let $selected = Math.floor(Math.random() * 27) + 1;
             cy.get('ul.content-results').find('li:nth-child(' + $selected + ')').then(($li) => {
                 if ($li.hasClass('banner')) {
@@ -53,60 +48,57 @@ context('Add to Favourites', () => {
 
                     })
                 });
+            }).then(()=>{
+                cy.visit("http://localhost:3000/hesabim/favorilerim/araclar").then(() => {
+                    cy.wait(3000).then(() => {
+                        var $count = 0;
+                        var $url = Cypress.env('faved_url');
+                        console.log($url);
+                        cy.get("ul.favorites-list li.list-item").each(($li) => {
+                            let $href = $li.find('.contentbox-innerwrap').attr('href');
+                            let n = $href.split("-");
+                            $href = n[n.length - 1];
+                            console.log('url: ' + $url)
+                            console.log('href: ' + $href)
+                            if ($url === $href) {
+                                $count++
+                            }
+                        }).then(() => {
+                            expect($count).to.equal(1);
+                        });
+
+                    });
+                })
+            }).then(()=>{
+                let $url = Cypress.env('url');
+                cy.visit($url).then(() => {
+                    cy.wait(3000).then(() => {
+                        cy.get(".fav-button-btn").click();
+                        cy.wait(1000);
+                    });
+                })
+            }).then(()=>{
+                cy.visit("http://localhost:3000/hesabim/favorilerim/araclar").then(() => {
+                    cy.wait(3000).then(() => {
+                        var $count = 0;
+                        var $url = Cypress.env('faved_url');
+                        console.log($url);
+                        cy.get("ul.favorites-list li.list-item").each(($li) => {
+                            let $href = $li.find('.contentbox-innerwrap').attr('href');
+                            let n = $href.split("-");
+                            $href = n[n.length - 1];
+                            console.log('url: ' + $url)
+                            console.log('href: ' + $href)
+                            if ($url === $href) {
+                                $count++
+                            }
+                        }).then(() => {
+                            expect($count).to.equal(0);
+                        });
+                    })
+                })
             });
 
         });
-        it('should() - have the favorited car in favorites', () => {
-            cy.visit("http://localhost:3000/hesabim/favorilerim/araclar").then(() => {
-                cy.wait(3000).then(() => {
-                    var $count = 0;
-                    var $url = Cypress.env('faved_url');
-                    console.log($url);
-                    cy.get("ul.favorites-list li.list-item").each(($li) => {
-                        let $href = $li.find('.contentbox-innerwrap').attr('href');
-                        let n = $href.split("-");
-                        $href = n[n.length - 1];
-                        console.log('url: ' + $url)
-                        console.log('href: ' + $href)
-                        if ($url === $href) {
-                            $count++
-                        }
-                    }).then(() => {
-                        expect($count).to.equal(1);
-                    });
-
-                });
-            })
-        });
-        it('should() - Remove the car from favorites list', () => {
-            let $url = Cypress.env('url');
-            cy.visit($url).then(() => {
-                cy.wait(3000).then(() => {
-                    cy.get(".fav-button-btn").click();
-                    cy.wait(1000);
-                });
-            })
-        });
-        it('should() - not show the removed car in the favorites list', () => {
-            cy.visit("http://localhost:3000/hesabim/favorilerim/araclar").then(() => {
-                cy.wait(3000).then(() => {
-                    var $count = 0;
-                    var $url = Cypress.env('faved_url');
-                    console.log($url);
-                    cy.get("ul.favorites-list li.list-item").each(($li) => {
-                        let $href = $li.find('.contentbox-innerwrap').attr('href');
-                        let n = $href.split("-");
-                        $href = n[n.length - 1];
-                        console.log('url: ' + $url)
-                        console.log('href: ' + $href)
-                        if ($url === $href) {
-                            $count++
-                        }
-                    }).then(() => {
-                        expect($count).to.equal(0);
-                    });
-                })
-            })
-        })
     });
 });
