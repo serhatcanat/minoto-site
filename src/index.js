689ce00a7926e5593e8dac6f88bb6afac9ce5f7d
taha_cankurt <cankurt.taha@gmail.com>
2020-03-17 15:09:03 +0300

    Product pagination page count fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import store from './data/store'
-import {Provider} from 'react-redux'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from "./data/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
 // Styles
-import './assets/scss/main.scss';
-
+import "./assets/scss/main.scss";
 ReactDOM.render(
-
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/services/LvpService.js
+++ src/services/LvpService.js
@@ -1,8 +1,6 @@
 // import * as client from "./Client";
 // import UrlAssembler from "url-assembler";
 // import {GET_VEHICLE_DETAIL_ENDPOINT} from "./apiEndpoints";
-
-
 export class LvpService {
     detail = (product) => {
         return product
